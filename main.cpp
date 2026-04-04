237cd402444d7e3ff9a40313159188ae70bea4a0
Chhetri-Aayush <aayush.chhetri.81@gmail.com>
2026-04-04 23:33:32 +0545

    updated

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <iostream>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 int main() {
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     std::cerr << "Problem while creation of the socket: " << strerror(errno)
               << "\n";
+    return 1;
   }
 
-  sockaddr_in serverAddress;
+  sockaddr_in serverAddress{};
+
   serverAddress.sin_family = AF_INET;
   serverAddress.sin_port = htons(8080);
   serverAddress.sin_addr.s_addr = INADDR_ANY;
@@ -19,9 +23,31 @@ int main() {
       0) {
     std::cerr << "three is some problem while binding: " << strerror(errno)
               << "\n";
+    return 1;
   }
 
-  listen(sockfd, 5);
+  if (listen(sockfd, 5) < 0) {
+
+    std::cerr << "three is some problem while listening: " << strerror(errno)
+              << "\n";
+    return 1;
+  }
+
+  while (true) {
+
+    sockaddr_in clientAddress{};
+    socklen_t client_len = sizeof(clientAddress);
+
+    int clientfd =
+        accept(sockfd, (struct sockaddr *)&clientAddress, &client_len);
+
+    if (clientfd < 0) {
+      std::cerr << "accept failed: " << strerror(errno) << "\n";
+    }
+    std::cout << "client connected" << "\n";
+    close(clientfd);
+  }
+  close(sockfd);
 
   return 0;
 }
